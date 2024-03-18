45330833ba0bcce065038ddd948a762b35010ad3
SantVilz <santiagovil19@gmail.com>
2024-03-17 20:34:01 -0500

    update serviceData + img

--- src/servicesData.js
+++ src/servicesData.js
@@ -1,58 +1,77 @@
+import img1 from './assets/svg/img 1.jpeg';
+import img2 from './assets/svg/img 2.jpeg';
+import img3 from './assets/svg/img 3.jpeg';
+import img4 from './assets/svg/img 4.jpeg';
+import img5 from './assets/svg/img 5.jpeg';
+import img6 from './assets/svg/img 6.jpeg';
+import img7 from './assets/svg/img 7.jpeg';
+import img8 from './assets/svg/img 8.jpeg';
+import img9 from './assets/svg/img 9.jpeg';
+
 const services = [
-    {
-      title: "Premium Wash",
-      description: "Exterior cleaning, shampooing with wax, tire cleaning and conditioning, thorough vacuuming, cleaning and conditioning of plastic panels, window cleaning, and application of air freshener. The Engine Bay detailing includes cleaning of plastic components, desalination and decontamination of oil and dust, conditioning of plastic parts, and air drying to ensure a complete refresh.",
-      price: "$55.00 - $65.00",
-      duration: "1 Hour"
-    },
-    {
-      title: "Interior Detail",
-      description: "Intensive cleaning tailored by fabric specialists, including stain removal and cleaning, enhancing the interior appearance of the car.",
-      price: "$50.00",
-      duration: "45 min"
-    },
-    {
-      title: "Lights Restoration",
-      description: "Decontamination in two steps followed by machine polishing.",
-      price: "$95.00",
-      duration: "1 Hour"
-    },
-    {
-      title: "Decontamination",
-      description: "Sealed in two steps with clear coat.",
-      price: "$50.00",
-      duration: "45 min"
-    },
-    {
-      title: "Wheels cleaning",
-      description: "Dismantling of all four tires for thorough cleaning. This process includes cleaning the brakes and fender liners for comprehensive maintenance.",
-      price: "$120.00",
-      duration: "1 Hour"
-    },
-    {
-      title: "Lux Shine",
-      description: "Polishing in three steps, including micro-scratch removal and enhancement of shine for a rejuvenated appearance.",
-      price: "$80.00",
-      duration: "1 Hour"
-    },
-    {
-      title: "Clay X Wax",
-      description: "Ideal for paint with orange peel effect or contamination, finished with a layer of carnauba wax to provide water repellency.",
-      price: "$160.00 - $220.00",
-      duration: "2 Hour - 3 Hours"
-    },
-    {
-      title: "Wheels Painting",
-      description: "Wheel painting rejuvenates and protects vehicle wheels with high-quality paint, offering customization and protection against damage. The service includes cleaning, sanding, priming, color painting, and finishing with a protective clear coat for durability and shine.",
-      price: "Starting price: $220",
-      duration: "1-2 Days"
-    },
-    {
-      title: "Calipers Painting",
-      description: "Caliper painting adds a custom look and protection to brake calipers with high-temperature resistant paint, involving cleaning, priming, color application, and a durable clear coat for resilience against heat and corrosion.",
-      price: "Starting price: $250",
-      duration: "1-2 Days"
-    }
-  ];
-  
+  {
+    title: "Premium Wash",
+    description: "Exterior cleaning, shampooing with wax, tire cleaning and conditioning, thorough vacuuming, cleaning and conditioning of plastic panels, window cleaning, and application of air freshener. The Engine Bay detailing includes cleaning of plastic components, desalination and decontamination of oil and dust, conditioning of plastic parts, and air drying to ensure a complete refresh.",
+    price: "$55.00 - $65.00",
+    duration: "1 Hour",
+    image: img1,
+  },
+  {
+    title: "Interior Detail",
+    description: "Intensive cleaning tailored by fabric specialists, including stain removal and cleaning, enhancing the interior appearance of the car.",
+    price: "$50.00",
+    duration: "45 min",
+    image: img2,
+  },
+  {
+    title: "Lights Restoration",
+    description: "Decontamination in two steps followed by machine polishing.",
+    price: "$95.00",
+    duration: "1 Hour",
+    image: img3,
+  },
+  {
+    title: "Decontamination",
+    description: "Sealed in two steps with clear coat.",
+    price: "$50.00",
+    duration: "45 min",
+    image: img4,
+  },
+  {
+    title: "Wheels cleaning",
+    description: "Dismantling of all four tires for thorough cleaning. This process includes cleaning the brakes and fender liners for comprehensive maintenance.",
+    price: "$120.00",
+    duration: "1 Hour",
+    image: img5,
+  },
+  {
+    title: "Lux Shine",
+    description: "Polishing in three steps, including micro-scratch removal and enhancement of shine for a rejuvenated appearance.",
+    price: "$80.00",
+    duration: "1 Hour",
+    image: img6,
+  },
+  {
+    title: "Clay X Wax",
+    description: "Ideal for paint with orange peel effect or contamination, finished with a layer of carnauba wax to provide water repellency.",
+    price: "$160.00 - $220.00",
+    duration: "2 Hour - 3 Hours",
+    image: img7,
+  },
+  {
+    title: "Wheels Painting",
+    description: "Wheel painting rejuvenates and protects vehicle wheels with high-quality paint, offering customization and protection against damage. The service includes cleaning, sanding, priming, color painting, and finishing with a protective clear coat for durability and shine.",
+    price: "Starting price: $220",
+    duration: "1-2 Days",
+    image: img8,
+  },
+  {
+    title: "Calipers Painting",
+    description: "Caliper painting adds a custom look and protection to brake calipers with high-temperature resistant paint, involving cleaning, priming, color application, and a durable clear coat for resilience against heat and corrosion.",
+    price: "Starting price: $250",
+    duration: "1-2 Days",
+    image: img9,
+  }
+];
+
 export default services;
