f369806a16fc02a35c9c55424c18a8841d587641
SantVilz <santiagovil19@gmail.com>
2024-04-06 10:42:38 -0500

    more images

--- src/servicesData.js
+++ src/servicesData.js
@@ -1,76 +1,57 @@
-import img1 from './assets/svg/img 1.jpeg';
-import img2 from './assets/svg/img 2.jpeg';
-import img3 from './assets/svg/img 3.jpeg';
-import img4 from './assets/svg/img 4.jpeg';
-import img5 from './assets/svg/img 5.jpeg';
-import img6 from './assets/svg/img 6.jpeg';
-import img7 from './assets/svg/img 7.jpeg';
-import img8 from './assets/svg/img 8.jpeg';
-import img9 from './assets/svg/img 9.jpeg';
-
 const services = [
   {
     title: "Premium Wash",
     description: "Elevate your vehicle's radiance with our meticulous Premium Wash. Our expert detailers meticulously cleanse every inch, restoring a showroom-like shine. From a thorough exterior shampooing to a luxurious wax application, we bring your car's beauty back to life. Immaculate interiors, pristine windows, and a refreshing fragrance complete this transformative experience.",
     price: "$55.00 - $65.00",
     duration: "1 Hour",
-    image: img1,
   },
   {
     title: "Interior Detail",
     description: "Indulge in a revitalizing Interior Detail, where our fabric specialists work their magic. Stains and blemishes disappear, unveiling the pristine beauty of your car's interior. With meticulous attention to detail, we restore the inviting ambiance you deserve, leaving your cabin looking and feeling like new.",
     price: "$50.00",
     duration: "45 min",
-    image: img2,
   },
   {
     title: "Lights Restoration",
     description: "Illuminate your journey with our Lights Restoration service. Our skilled technicians meticulously revive dull and cloudy headlights, restoring their crystal-clear brilliance. Experience enhanced visibility and a newfound sense of confidence on the road, as your vehicle's eyes regain their youthful glow.",
     price: "$95.00",
     duration: "1 Hour",
-    image: img3,
   },
   {
     title: "Decontamination",
     description: "Protect your vehicle's pristine finish with our comprehensive Decontamination service. Our expert detailers employ a meticulous two-step process, removing embedded contaminants and sealing your car's paint with a protective clear coat. Embrace the confidence of a flawless, showroom-quality finish that withstands the elements.",
     price: "$50.00",
     duration: "45 min",
-    image: img4,
   },
   {
     title: "Wheels Cleaning",
     description: "Unlock the true potential of your vehicle's wheels with our thorough Wheels Cleaning service. Our dedicated technicians meticulously dismantle and clean each wheel, ensuring every nook and cranny is restored to pristine condition. From brakes to fender liners, no detail is overlooked in this comprehensive maintenance process.",
     price: "$120.00",
     duration: "1 Hour",
-    image: img5,
   },
   {
     title: "Lux Shine",
     description: "Embrace the ultimate in automotive indulgence with our Lux Shine service. Our skilled detailers employ a meticulous three-step polishing process, meticulously removing micro-scratches and restoring your vehicle's paint to a breathtaking, mirror-like shine. Prepare to turn heads and bask in the envy of onlookers as your car radiates a rejuvenated, showroom-fresh appearance.",
     price: "$80.00",
     duration: "1 Hour",
-    image: img6,
   },
   {
     title: "Clay X Wax",
     description: "Unveil your vehicle's true potential with our Clay X Wax treatment. Designed for paint afflicted by orange peel or contamination, our expert detailers gently eliminate imperfections, leaving a smooth, flawless canvas. A final layer of premium carnauba wax provides a stunning, water-repellent finish that will leave your car gleaming with renewed brilliance.",
     price: "$160.00 - $220.00",
     duration: "2 Hour - 3 Hours",
-    image: img7,
   },
   {
     title: "Wheels Painting",
     description: "Transform your vehicle's wheels into showstoppers with our Wheels Painting service. Our skilled technicians meticulously rejuvenate and protect your wheels with high-quality paint, offering a world of customization options. From cleaning and sanding to priming and color application, each step is executed with precision. A durable clear coat ensures lasting protection against damage, ensuring your wheels remain as striking as the day they were painted.",
     price: "Starting price: $220",
     duration: "1-2 Days",
-    image: img8,
   },
   {
     title: "Calipers Painting",
     description: "Unlock a new level of automotive style with our Calipers Painting service. Our experts masterfully transform your brake calipers into eye-catching accents, using high-temperature resistant paint for a custom look that endures. From thorough cleaning and priming to precise color application and a durable clear coat, we ensure your calipers not only look stunning but also withstand the rigors of heat and corrosion.",
     price: "Starting price: $250",
     duration: "1-2 Days",
-    image: img9,
   }
 ];
 
